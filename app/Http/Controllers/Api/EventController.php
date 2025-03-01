ec361dcf784b04c46235cbae13e1c51f1c04a37b
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-03-01 22:38:15 +0100

    testing apis

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Event::all();  # use Postman software with GET url http://127.0.0.1:8000/api/events
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return $event;  #Postman GET url http://127.0.0.1:8000/api/events/22
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
